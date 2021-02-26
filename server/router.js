3657eea322a60fc422f0c227c1d4cff64e6a45bc
rdnine <rafael@rdnine.dev>
2021-02-26 19:33:04 +0000

    feat: request to external api
    GET request to openweathermaps api

--- server/router.js
+++ server/router.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { get } from './services/weather.js'
+import { getWeather } from './services/weather.js'
 
 // Express object instance
 const router = express();
 
 router.get('/', (req, res) => res.send('App is working!'))
 
-router.get('/api/v1/weather/city/:name', get)
+router.get('/api/v1/weather/city/:name', getWeather)
 
 export default router
--- server/services/weather.js
+++ server/services/weather.js
@@ -1,5 +1,16 @@
 import axios from 'axios'
+import config from 'config'
 
-export const get = (req, res) => {
-  res.send(`GET/ ${req.params.name} weather`)
+export const getWeather = (req, res) => {
+
+  // Get request to openweathermaps api - Current weather endpoint
+  axios.get(
+    `${config.get('api.endpoint')}?q=${req.params.name}&appid=${config.get('api.key')}&units=metric`
+  ).then(response => {
+    console.log(response)
+    res.send(response.data)
+  }).catch(err => {
+    res.send(err.data)
+  })
+
 }
