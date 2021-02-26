1d7f23e5ccae01b7742e6af744f47bcc7519941b
rdnine <rafael@rdnine.dev>
2021-02-26 17:26:44 +0000

    feat: added compression middleware to server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 import express from 'express'
 import config from 'config'
 import cors from 'cors'
+import compression from 'compression'
 
+// Create object from the express module
 const server = express()
 
+// Enable All CORS Requests
 server.use(cors())
 
+// Enable gzip compression for all responses
+server.use(compression())
+
+// Server routes
 server.get('/', (req, res) => res.send('App is working!'))
 
+// Server listens on the designated port
 server.listen(
   config.get('server.port'),
   () => console.log(`Server running on port: http://localhost:${config.get('server.port')}`)
