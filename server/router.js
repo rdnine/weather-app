6e2ed87580ad53dd84f197173993ea1154a212ac
rdnine <rafael@rdnine.dev>
2021-02-26 19:25:15 +0000

    feat: update router

--- server/router.js
+++ server/router.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import { get } from './services/weather.js'
 
 // Express object instance
 const router = express();
 
 router.get('/', (req, res) => res.send('App is working!'))
+
+router.get('/api/v1/weather/city', get)
 
 export default router
