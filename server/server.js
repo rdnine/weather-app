fdd3c09670b6f392598b8b2fc75f12ecc835a57c
rdnine <rafael@rdnine.dev>
2021-02-26 17:48:36 +0000

    feat: added body parser middleware

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import config from 'config'
 import cors from 'cors'
 import compression from 'compression'
+import bodyParser from 'body-parser'
 
 // Create object from the express module
 const server = express()
@@ -11,6 +12,9 @@ server.use(cors())
 
 // Enable gzip compression for all responses
 server.use(compression())
+
+// Parse incoming request to json
+server.use(bodyParser.json())
 
 // Server routes
 server.get('/', (req, res) => res.send('App is working!'))
