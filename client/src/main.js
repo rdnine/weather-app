0b18596a0d22617357d8e31061700ebb8ae58443
rdnine <rafael@rdnine.dev>
2021-02-27 18:31:45 +0000

    feat: upgrade app
    Added tailwind and scss

--- client/src/main.js
+++ client/src/main.js
@@ -3,4 +3,7 @@ import App from './containers/App.vue'
 import router from './router'
 import store from './store'
 
+import './styles/scss/base/tailwind.scss'
+import './styles/scss/global.scss'
+
 createApp(App).use(store).use(router).mount('#app')
