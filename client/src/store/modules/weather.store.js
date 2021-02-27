e674ab531a5223deefa85a4c98d41a9894c06c2a
rdnine <rafael@rdnine.dev>
2021-02-27 23:47:01 +0000

    feat: update actions

--- client/src/store/modules/weather.store.js
+++ client/src/store/modules/weather.store.js
@@ -1,6 +1,46 @@
-const state = {}
-const getters = {}
-const mutations = {}
-const actions = {}
+import axios from 'axios'
+
+const state = {
+  cities: [],
+  hasCities: false
+}
+const getters = {
+  cities: state => state.cities,
+  hasCities: state => state.hasCities
+}
+
+const mutations = {
+  UPDATE_CITIES(state, value) {
+    state.cities = value
+  },
+  UPDATE_STATUS(state, value) {
+    state.hasCities = value;
+    console.log(state)
+  },
+  RESET(state) {
+    state.cities = [];
+    state.hasCities = false;
+  }
+}
+
+const actions = {
+  getCitiesList({ commit }, payload) {
+    axios
+      .get(`http://localhost:8081/api/v1/weather/cities/${payload.value}`)
+      .then(response => {
+        if (response.status === 200) {
+          commit('UPDATE_CITIES', response.data.data.list)
+          commit('UPDATE_STATUS', true)
+        }
+      })
+      .catch(err => {
+        console.log(err.response.data.message)
+        commit('UPDATE_STATUS', true)
+      })
+  },
+  clearResults({ commit }) {
+    commit('RESET')
+  }
+}
 
 export default { state, getters, mutations, actions }
