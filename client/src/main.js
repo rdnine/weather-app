4adf09e80a3f702f845073c301b1a31cfe6463c7
rdnine <rafael@rdnine.dev>
2021-02-27 21:24:46 +0000

    feat: use axios

--- client/src/main.js
+++ client/src/main.js
@@ -5,11 +5,15 @@ import store from './store'
 
 import PrimeVue from 'primevue/config'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import './styles/scss/base/tailwind.scss'
 import './styles/scss/global.scss'
 
 const app = createApp(App)
 
+app.use(VueAxios, axios)
 app.use(PrimeVue)
 
 app.use(store)
