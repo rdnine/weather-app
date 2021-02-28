f4e27b0de6c6588166f519666b4dd6f89af993f3
rdnine <rafael@rdnine.dev>
2021-02-28 22:10:57 +0000

    feat: weather store update
    Can now add cities from search

--- client/src/store/modules/weather.store.js
+++ client/src/store/modules/weather.store.js
@@ -1,45 +1,82 @@
 import axios from 'axios'
 
 const state = {
-  cities: [],
+  cities: {
+    list: [],
+    results: []
+  },
+  hasResults: false,
   hasCities: false
 }
 const getters = {
-  cities: state => state.cities,
-  hasCities: state => state.hasCities
+  results: state => state.cities.results,
+  list: state => state.cities.list,
+  hasCities: state => state.hasCities,
+  hasResults: state => state.hasResults
 }
 
 const mutations = {
-  UPDATE_CITIES(state, value) {
-    state.cities = value
+  ADD_TO_LIST(state, value) {
+    state.cities.list.push(value)
   },
-  UPDATE_STATUS(state, value) {
+  REMOVE_FROM_LIST(state, value) {
+    const newList = state.cities.list.filter(el => el.id !== value)
+
+    if(!newList.length > 0) {
+      state.hasCities = false;
+    }
+    state.cities.list = newList;
+  },
+  UPDATE_RESULT(state, value) {
+    state.cities.results = value
+  },
+  UPDATE_LIST_STATUS(state, value) {
     state.hasCities = value;
-    console.log(state)
   },
-  RESET(state) {
-    state.cities = [];
-    state.hasCities = false;
+  UPDATE_RESULT_STATUS(state, value) {
+    state.hasResults = value;
+  },
+  RESET_RESULTS(state) {
+    state.cities.results = [];
+    state.hasResults = false;
   }
 }
 
 const actions = {
-  getCitiesList({ commit }, payload) {
+  getCities({ commit }, payload) {
     axios
       .get(`http://localhost:8081/api/v1/weather/cities/${payload.value}`)
       .then(response => {
         if (response.status === 200) {
-          commit('UPDATE_CITIES', response.data.data.list)
-          commit('UPDATE_STATUS', true)
+          commit('UPDATE_RESULT', response.data.data.list)
+          commit('UPDATE_RESULT_STATUS', true)
         }
       })
       .catch(err => {
         console.log(err.response.data.message)
-        commit('UPDATE_STATUS', true)
+        commit('UPDATE_RESULT_STATUS', true)
       })
   },
+  addCity({ commit }, payload) {
+    axios.get(`http://localhost:8081/api/v1/weather/city/id/${payload}`)
+    .then(response => {
+      if (response.status === 200) {
+        commit('ADD_TO_LIST', response.data.data)
+        commit('UPDATE_LIST_STATUS', true)
+        commit('RESET_RESULTS')
+      }
+    })
+    .catch(err => {
+      console.log(err.response.data.message)
+    })
+  },
+  removeCity({ commit }, payload) {
+    if(payload) {
+      commit('REMOVE_FROM_LIST', payload)
+    }
+  },
   clearResults({ commit }) {
-    commit('RESET')
+    commit('RESET_RESULTS')
   }
 }
 
