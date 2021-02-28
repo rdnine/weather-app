0350f62abaa8cca22e5a893c05242f781da37b34
rdnine <rafael@rdnine.dev>
2021-02-28 18:34:08 +0000

    feat: new api resources

--- server/routes/api/weather.js
+++ server/routes/api/weather.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { getWeather, getCities } from '../../services/weather.js'
+import { getWeather, getWeatherByID, getCities } from '../../services/weather.js'
 
 const weather = express.Router()
 
 weather.get('/city/:name', getWeather)
+weather.get('/city/id/:id', getWeatherByID)
+
 weather.get('/cities/:name', getCities)
 
 export default weather
